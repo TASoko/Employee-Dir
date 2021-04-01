ae90cbeb0acd2f2c3f55b2e977b77e5eab0364cd
Thoko <thokozile.soko@yahoo.com>
2021-03-31 20:22:53 -0400

    I creacted a search component and used a boostrap input.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import DirectoryTitle from "./components/Title";
 import Table from "./components/Table";
+import Search from "./components/Search"
 
 function App() {
   return (
     <div>
       <DirectoryTitle />
+      <Search/>
       <Table />
     </div>
   );
---
+++ src/components/Search.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Search() {
+
+   //Things I need
+   //Something to reference names(first and last) - some form of matching
+   //Something to reference country - some form of matching 
+  return (
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder=""
+        aria-label="Example text with button addon"
+        aria-describedby="button-addon1"
+      />
+  );
+}
+
+export default Search;
